b865ec56eebd7e80bd96d09855efb36670eb53f6
CaoMeiYouRen <caomeiyouren@qq.com>
2026-09-15 01:24:25 +0800

    fix(docs): 文档站按页面语言注入组件内建文案
    - 文档站主题接入 CaomeiConfigProvider，按 VitePress 页面语言注入 zh-CN / en-US
    - 英文页的组件内建文案（空态、可访问名等）不再显示中文

--- docs/.vitepress/theme/layout.vue
+++ docs/.vitepress/theme/layout.vue
@@ -1,20 +1,30 @@
 <script setup lang="ts">
+import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import { computed } from 'vue'
+import { CaomeiConfigProvider } from '../../../src/components/config-provider'
 import { CaomeiConfirmDialog } from '../../../src/components/confirm-dialog'
 import { CaomeiToastProvider } from '../../../src/components/toast'
+import type { CaomeiLocale } from '../../../src/locale'
 import ThemePresetSwitcher from './components/theme-preset-switcher.vue'
 
 const { Layout } = DefaultTheme
+
+/** 文档站按页面语言注入组件内建文案（当前仅内建 zh-CN / en-US 两种，其余回落 zh-CN） */
+const { lang } = useData()
+const locale = computed<CaomeiLocale>(() => (lang.value === 'en-US' ? 'en-US' : 'zh-CN'))
 </script>
 
 <template>
-    <CaomeiToastProvider>
-        <CaomeiConfirmDialog>
-            <Layout>
-                <template #nav-bar-content-after>
-                    <ThemePresetSwitcher />
-                </template>
-            </Layout>
-        </CaomeiConfirmDialog>
-    </CaomeiToastProvider>
+    <CaomeiConfigProvider :locale="locale">
+        <CaomeiToastProvider>
+            <CaomeiConfirmDialog>
+                <Layout>
+                    <template #nav-bar-content-after>
+                        <ThemePresetSwitcher />
+                    </template>
+                </Layout>
+            </CaomeiConfirmDialog>
+        </CaomeiToastProvider>
+    </CaomeiConfigProvider>
 </template>

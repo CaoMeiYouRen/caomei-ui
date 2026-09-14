5bf002d81a6fc4cb837aa70720bfeecfe004fa25
CaoMeiYouRen <caomeiyouren@qq.com>
2026-09-15 02:10:43 +0800

    test(vitest): 更新覆盖报告格式以包含更多信息
    - 将覆盖报告的格式更新为包含 'json-summary' 和 'lcov'

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         passWithNoTests: true,
         coverage: {
             provider: 'v8',
-            reporter: ['text', 'html'],
+            reporter: ['text', 'html', 'json-summary', 'lcov'],
             include: ['src/**'],
         },
     },
